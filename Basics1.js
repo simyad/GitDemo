2175043a638306f1a8fe4a30c3e7eb9abddb7421
simple <simpleyadav_123@yahoo.com>
2026-01-05 18:37:45 -0800

    first commit

---
+++ Basics1.js
@@ -0,0 +1,13 @@
+let one = [12,3,5,7]
+let sum = 0
+//console.log("array is" + one)
+for (let i=0 ; i < one.length ; i++){
+    sum = sum + one[i]
+   // console.log("sum inside lopp is " + sum)
+}
+console.log("sum is " +sum)
+
+let total = one.reduce((sum,two)=>sum+two,0)
+console.log("total is ", total)
+let newArray = one.filter(one=>one%2==0)
+console.log("new array is " + newArray)
